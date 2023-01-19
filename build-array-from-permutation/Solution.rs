ce87e98c49d1c5b8458617d657dd29f7ac2db8ba
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2023-01-19 21:39:45 +0530

    Build Array from Permutation done leetcode

---
+++ build-array-from-permutation/Solution.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+        let mut v:Vec<i32> = Vec::new();
+        for num in nums.iter() {
+         v.push(nums[*num as usize]);
+        }
+        v
+    }
+}
